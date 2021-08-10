8f6685892aed962db821aa9920e45d41fe5a835a
Juliette.Wang <nannan.wang@broadlink.com.cn>
2021-08-10 10:28:15 +0800

    修改loading显示；

--- src/broadlink-jssdk/dna/adapter.js
+++ src/broadlink-jssdk/dna/adapter.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-29 11:22:41
- * @LastEditTime: 2021-08-10 10:23:09
+ * @LastEditTime: 2021-08-10 10:26:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \tcl\src\broadlink-jssdk\dna\adapter.js
@@ -79,7 +79,7 @@ const platformSDK = (() => {
   };
 })();
 
-const ready = () => deviceInfoPromise.then((data) => ({ ready: data }));
+const ready = () => deviceInfoPromise.then((data) => ({ status: data }));
 
 export default {
   platform: 'dna',
